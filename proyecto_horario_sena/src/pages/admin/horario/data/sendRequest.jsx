94b752f7f44fef633ce7d6fe6ec6b1f1e46866e4
Alejandro-RECO <130815506+Alejandro-RECO@users.noreply.github.com>
2023-12-07 00:32:10 -0500

    Generating component with FK relation block

--- proyecto_horario_sena/src/pages/admin/horario/data/sendRequest.jsx
+++ proyecto_horario_sena/src/pages/admin/horario/data/sendRequest.jsx
@@ -1,59 +1,75 @@
-import { API_URL } from "../../../../utils/httpRequest";
 import { TOKEN } from "../../../../utils/httpRequest";
-import axios from "axios";
+import { getDataFromEndpoin } from "../../../../utils/httpRequest";
+import { useEffect, useState } from 'react';
 
-const endpoint = 'bloque'
+export const BloqueData = ()=>{
 
-export const getDataHorario = async () => {
-  if (!TOKEN) {
-    window.location.href = '/login';
-    return null;
+  if (!TOKEN){
+    window.location.href = '/'
+
+    return null
   }
+  // Estados  que almacenan la infromación 
+  const [datosBloque, setDatoBloque] = useState({ bloques: [] })
+  const [datosAmbiente, setDatosAmbiente] = useState({ ambientes: [] })
+  const [datosFicha, setDatosFicha] = useState({ fichas: [] })
+  const [datosInstructor, setDatosInstructor] = useState({ instructores: [] })
+  const [datosJornada, setDatosJornada] = useState({ tipoJornada: [] })
+  const [datosSede, setDatosSede] = useState({ sedes: [] })
+  const [datosTematica, setDatosTematica] = useState({ tematica: [] })
+  const [datosTrimestre, setDatosTrimestre] = useState({ trimestres: [] })
 
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${TOKEN}`,
-  };
+  const token = TOKEN
 
-  try {
-    const response = await axios.get(`${API_URL}/${endpoint}/`, { headers });
-    return response.data;
-  } catch (error) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const bloqueResponse = await getDataFromEndpoin('bloque', token)
+        setDatoBloque(bloqueResponse)
+        const ambienteResponse = await getDataFromEndpoin('ambiente', token)
+        setDatosAmbiente(ambienteResponse)
+        const fichaResponse = await getDataFromEndpoin('ficha', token)
+        setDatosFicha(fichaResponse)
+        const instructorResponse = await getDataFromEndpoin('instructor', token)
+        setDatosInstructor(instructorResponse)
+        const jornadaResponse = await getDataFromEndpoin('jornada', token)
+        setDatosJornada(jornadaResponse)
+        const sedeResponse = await getDataFromEndpoin('sede', token)
+        setDatosSede(sedeResponse)
+        const tematicaResponse = await getDataFromEndpoin('tematica', token)
+        setDatosTematica(tematicaResponse)
+        const trimestreResponse = await getDataFromEndpoin('trimestre', token)
+        setDatosTrimestre(trimestreResponse)
 
-    console.error("Error en la solicitud:", error);
-    throw error;
-  }
-};
-
-export const getDataMismos = async (
-  mismosData
-) => {
-  if (!TOKEN) {
-    window.location.href = '/login';
-    return null;
-  }
-
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${TOKEN}`,
-  };
-
-  try {
-    const response = await axios.get(`${API_URL}/${endpoint}/mismos/5/1`, { headers }, mismosData);
-    return response.data;
-  } catch (error) {
-    if (error.response && error.response.status === 401) {
-      if (!alertShown) {
-        // Podrías intentar renovar el token o redirigir al usuario a la página de inicio de sesión
-        // Si tu backend admite la renovación de tokens, podrías implementar esa lógica aquí
-        alert('La sesión ha caducado. Serás redirigido al inicio de sesión.');
-        alertShown = true; // Marcar que la alerta ha sido mostrada
-        window.location.href = '/login'; // Ajusta la URL según tu estructura de rutas
+      }catch (error) {
+        console.log("[ERROR FETCH DATA]", error);
       }
-      return null;
     }
 
-    console.error("Error en la solicitud:", error);
-    throw error;
-  }
-};
+    fetchData()
+  }, [])
+
+  const bloqueConInfo = datosBloque.bloques.map((bloque) => {
+    const ambienteInfo = datosAmbiente.ambientes.find((ambiente) => ambiente.idAmbiente === bloque.idAmbienteFK);
+    const fichaInfo = datosFicha.fichas.find((ficha) => ficha.idFicha === bloque.idFichaFK);
+    const instructorInfo = datosInstructor.instructores.find((instructor) => instructor.idInstructor === bloque.idInstructorFK);
+    const jornadaInfo = datosJornada.tipoJornada.find((jornada) => jornada.idJornada === bloque.idJornadaFK);
+    const sedeInfo = datosSede.sedes.find((sede) => sede.idSede === bloque.idSedeFK);
+    const tematicaInfo = datosTematica.tematica.find((tematica) => tematica.idProgramaFK === bloque.idTematicaFK);
+    const trimestreInfo = datosTrimestre.trimestres.find((trimestre) => trimestre.idTrimestre === bloque.idTrimestreFK);
+
+    return {
+      ...bloque,
+      ambienteInfo,
+      fichaInfo,
+      instructorInfo,
+      jornadaInfo,
+      sedeInfo,
+      tematicaInfo,
+      trimestreInfo,
+      
+    };
+  });
+  return bloqueConInfo;
+}
+
--- proyecto_horario_sena/src/utils/httpRequest.jsx
+++ proyecto_horario_sena/src/utils/httpRequest.jsx
@@ -5,11 +5,23 @@ export const API_URL = 'https://pqddmp2g-5000.use2.devtunnels.ms';
 
 // Función para realizar una solicitud GET a una URL configurable
 
-export const getDataFromEndpoin = async (endpoint) => {
+export const getDataFromEndpoin = async (endpoint, TOKEN ) => {
+  if (!TOKEN){
+    window.location.href = '/'
+
+    return null
+  }
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${TOKEN}`,
+  }
+
   try {
-    const response = await axios.get(`${API_URL}/${endpoint}`);
-    return response.data;
+    const response = await axios.get(`${API_URL}/${endpoint}/`, {headers})
+    return response.data
   } catch (error) {
+    console.log("Error en el fetch", error)
     throw error;
   }
 };
